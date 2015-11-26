d11899ea6e5fa5b564a1e22dc2f9ccd781babacd
tmbashir <ifarting@gmail.com>
2015-11-25 20:48:11 -0500

    minor change

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -12,19 +12,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var lyricsView: UITextView!
-    var attributedString = NSMutableAttributedString()
-    var finalSongString:String = ""
-    //Silly Song Template
+    let vowel = "aeiou\u{00E4}\u{00EB}\u{00EF}\u{00F6}\u{00DC}" // vowel and Diaeresis
+    let specialChar = "bfm" // special case
     var bananaFanaTemplate:String = ""
+    var specialSong:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //default template
         bananaFanaTemplate = [
             "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>", "\n",
             "Banana Fana Fo F<SHORT_NAME>","\n",
             "Me My Mo M<SHORT_NAME>","\n",
             "<FULL_NAME>"].joinWithSeparator("")
+        
+        //This is presented if name starts with "b", "f", "m"
+        specialSong = [
+            "<FULL_NAME>, <FULL_NAME>, Bo-<SHORT_NAME>", "\n",
+            "Banana Fana Fo-<SHORT_NAME>","\n",
+            "Me My Mo-<SHORT_NAME>","\n",
+            "<FULL_NAME>"].joinWithSeparator("")
     }
+    
     // MARK:- Action methods
     @IBAction func reset(sender: AnyObject) {
         nameField.text = ""
@@ -33,15 +42,14 @@ class ViewController: UIViewController {
     
     @IBAction func displayLyrics(sender: AnyObject) {
         let randNumber = Int(arc4random_uniform(6))
-        if let name = nameField.text{
+        if let name = nameField.text {
             if name.isEmpty {
                 lyricsView.font = UIFont(name: "Papyrus", size: 24.0)
                 lyricsView.text = "Name field can not be blank!"
                 
-            }else {
-                
+            } else {
                 let nameCount = name.characters.count
-                let song = lyricsFromName(bananaFanaTemplate, fullName: name.capitalizedString)
+                let song = lyricsFromName(name.capitalizedString)
                 attributedStyleString(nameCount, randNumber: randNumber, finalSong: song)
             }
         }
@@ -49,21 +57,12 @@ class ViewController: UIViewController {
     
     //MARK:- Helper methods
     
-    //ShortName returns short name by removing Consonant before first Vowel
     func shortName(name:String)-> String {
-        let vowel = "aeiou\u{00E4}\u{00EB}\u{00EF}\u{00F6}\u{00DC}"
-        let vowelCharSet = NSCharacterSet(charactersInString: vowel)
-        let lowerCaseName = name.lowercaseString
-        if let charRange = lowerCaseName.rangeOfCharacterFromSet(vowelCharSet) {
-            return lowerCaseName.substringFromIndex(charRange.startIndex)
-        }
-        return lowerCaseName
+        return findCharacterInRange(name)
     }
     
-    //lyricsFromName creates Silly Song from given name
-    func lyricsFromName(lyricTemplate: String, fullName: String) -> String {
-        let finalTemplate = lyricTemplate.stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName).stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortName(fullName))
-        return finalTemplate
+    func lyricsFromName(fullName: String) -> String {
+        return creatSongTemplateFromName(fullName)
     }
     
     //Dismiss keyboard
@@ -71,62 +70,114 @@ class ViewController: UIViewController {
         textField.resignFirstResponder()
         return true
     }
+
+    //Attributed string
+    func attributedStyleString(count:Int, randNumber:Int, finalSong:String){
+        let attributedString = NSMutableAttributedString(string: finalSong)
+        attributedString.addAttribute(NSFontAttributeName,
+            value: UIFont(
+            name:"BradleyHandITCTT-Bold",
+            size: 27.0)!,
+            range: NSRange(
+            location:count,
+            length: attributedString.length - count))
+        
+        attributedString.addAttribute(NSForegroundColorAttributeName,
+            value: color(randNumber),
+            range: NSRange(
+            location:0,
+            length: count))
+        
+        attributedString.addAttribute(NSForegroundColorAttributeName,
+            value: color(randNumber - 1),
+            range: NSRange(
+            location: count,
+            length:attributedString.length - count))
+        
+        attributedString.addAttribute(NSFontAttributeName,
+            value: UIFont(
+            name:"BradleyHandITCTT-Bold",
+            size: 28.0)!,
+            range: NSRange(
+            location:0,
+            length: count ))
+        
+        lyricsView.attributedText = attributedString
+        lyricsView.textAlignment = .Center
+        nameField.text = ""
+    }
+    
+    // returns short name
+    func findCharacterInRange(name:String)->String{
+        let vowelCharSet = NSCharacterSet(charactersInString: vowel)
+        let specialCharSet = NSCharacterSet(charactersInString: specialChar)
+        let lowerCaseName = name.lowercaseString
+        
+        //check if first letter is a, f, m
+        for char in lowerCaseName.characters {
+            switch char {
+            case "b":
+                if let charRange = lowerCaseName.rangeOfCharacterFromSet(specialCharSet) {
+                    return lowerCaseName.substringFromIndex(charRange.startIndex.successor())
+                }
+            case "f":
+                if let charRange = lowerCaseName.rangeOfCharacterFromSet(specialCharSet) {
+                    return lowerCaseName.substringFromIndex(charRange.startIndex.successor())
+                }
+            case "m":
+                if let charRange = lowerCaseName.rangeOfCharacterFromSet(specialCharSet) {
+                    return lowerCaseName.substringFromIndex(charRange.startIndex.successor())
+                }
+            default:
+                if let charRange = lowerCaseName.rangeOfCharacterFromSet(vowelCharSet) {
+                    return lowerCaseName.substringFromIndex(charRange.startIndex)
+                }
+            }
+        }
+        return lowerCaseName
+    }
+    
+    // returns Silly Song from given name
+    func creatSongTemplateFromName(fullName:String)->String{
+        let lowercaseFullName = fullName.lowercaseString
+        var finalTemplate:String = ""
+        for char in lowercaseFullName.characters {
+            switch char {
+            case "b":
+                finalTemplate = specialSong.stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName).stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortName(fullName))
+                return finalTemplate
+            case "f":
+                finalTemplate = specialSong.stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName).stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortName(fullName))
+                return finalTemplate
+            case "m":
+                finalTemplate = specialSong.stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName).stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortName(fullName))
+                return finalTemplate
+            default:
+                finalTemplate = bananaFanaTemplate.stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName).stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortName(fullName))
+            }
+        }
+        return finalTemplate
+    }
     
     //random text color
     func color(number:Int)->UIColor{
         switch number{
         case 0 :
-            return UIColor.redColor()
-        case 1:
-            return UIColor.greenColor()
-        case 2:
-            return UIColor.orangeColor()
-        case 3:
             return UIColor.whiteColor()
-        case 4:
+        case 1:
+            return UIColor.orangeColor()
+        case 2:
+            return UIColor.greenColor()
+        case 3:
             return UIColor.purpleColor()
+        case 4:
+            return UIColor.magentaColor()
         case 5:
-            return UIColor.yellowColor()
+            return UIColor.redColor()
+            
         default:
             return UIColor.lightTextColor()
         }
-    }
-    
-    //Attributed string
-    func attributedStyleString(count:Int, randNumber:Int, finalSong:String){
-        attributedString = NSMutableAttributedString(string: finalSong)
-        attributedString.addAttribute(NSFontAttributeName,
-            value: UIFont(
-                name:"BradleyHandITCTT-Bold",
-                size: 27.0)!,
-            range: NSRange(
-                location:count,
-                length: attributedString.length - count))
-        
-        attributedString.addAttribute(NSForegroundColorAttributeName,
-            value: color(randNumber),
-            range: NSRange(
-                location:0,
-                length: count))
-        
-        attributedString.addAttribute(NSForegroundColorAttributeName,
-            value: color(randNumber - 1),
-            range: NSRange(
-                location: count,
-                length:attributedString.length - count))
-        
-        attributedString.addAttribute(NSFontAttributeName,
-            value: UIFont(
-                name:"BradleyHandITCTT-Bold",
-                size: 28.0)!,
-            range: NSRange(
-                location:0,
-                length: count ))
-        
-        lyricsView.attributedText = attributedString
-        lyricsView.textAlignment = .Center
-        nameField.text = ""
-
     }
 }
 
